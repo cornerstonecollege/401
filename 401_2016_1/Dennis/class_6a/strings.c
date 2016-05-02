ab8a9ce00dc47fe25eb7518f67495253c2397678
makabex <dennis_pham@hotmail.com>
2016-05-02 13:29:06 -0700

    class 6a

---
+++ 401_2016_1/Dennis/class_6a/strings.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char **argv)
+{ 
+  int sizeOfString;
+  printf("what size of string do you need");
+  scanf("%d", &sizeOfString);
+
+  char * str =  malloc(sizeof(char) * sizeOfString);
+  
+  printf("Type the string: ");
+  scanf("%s", str);
+
+  printf("The string is: %s\n", str);
+
+  free(str);
+  return 0;
+
+
+
+
+
+}
