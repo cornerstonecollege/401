d6c484180a971646a703bba5b12d4cc7b5f65704
Dennis <dennis_pham@hotmail.com>
2016-05-10 13:07:15 -0700

    class 11

---
+++ 401_2016_1/Dennis/class_11/Class_11/Class_11/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Class_11
+//
+//  Created by Dennis Pham on 2016-05-10.
+//  Copyright © 2016 Dennis Pham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool
+    {
+        
+      NSString *name = @"Digby";
+     
+      NSLog(@"%@", name);
+    }
+    return 0;
+}
