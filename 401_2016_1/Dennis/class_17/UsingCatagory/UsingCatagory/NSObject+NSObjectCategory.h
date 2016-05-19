334dec2db1fced4a78e07bafaf7e6c020b461b1c
Dennis <dennis_pham@hotmail.com>
2016-05-19 13:02:30 -0700

    class 17

---
+++ 401_2016_1/Dennis/class_17/UsingCatagory/UsingCatagory/NSObject+NSObjectCategory.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+NSObjectCategory.h
+//  UsingCatagory
+//
+//  Created by Dennis Pham on 2016-05-19.
+//  Copyright © 2016 Dennis Pham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (NSObjectCategory)
+
+- (NSString *) toString;
+
+@end
