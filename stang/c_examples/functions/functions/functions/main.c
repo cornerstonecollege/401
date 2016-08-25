30077454d07fd34c4d9a34ef9159a3664fa114db
SG5X <sglodowski@aol.pl>
2016-08-24 17:06:29 -0700

    learn a little bit about functions

---
+++ stang/c_examples/functions/functions/functions/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  functions
+//
+//  Created by SG on 2016-08-24.
+//  Copyright © 2016 SG. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    printf("ddgbd");
+    
+    
+    return 0;
+}
