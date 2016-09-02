020faa4a21db604eae21b8b457fb2487815fd990
Jorge Ardila <jorgeardila007@gmail.com>
2016-09-02 14:58:14 -0700

    c:

---
+++ jorge_ardila/c_examples/RealCPrograming/RealCPrograming/Calculator.c
@@ -0,0 +1,29 @@
+//
+//  Calculator.c
+//  RealCPrograming
+//
+//  Created by jorgelocal on 2016-08-31.
+//  Copyright © 2016 jorgelocal. All rights reserved.
+//
+
+#include "Calculator.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int mult(int a, int b)
+{
+    return a * b;
+}
+
+int div(int a, int b)
+{
+    return a / b;
+}
---
+++ jorge_ardila/c_examples/RealCPrograming/RealCPrograming/main.c
@@ -0,0 +1,10 @@
+
+
+#include <stdio.h>
+#include "Calculator.h"
+
+int main(int argc, const char * argv[]) {
+   
+    printf("The sum is %i\n", Calculator_add(2, 6));
+    return 0;
+}
