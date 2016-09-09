cb8a32d0f35d790a813e83ce734c797c93bc1f9b
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-09-09 14:21:33 -0700

    Class about property modifiers

---
+++ 401_2016_2/objective_c_examples/Onwership/Onwership/Triangle.m
@@ -0,0 +1,18 @@
+//
+//  Triangle.m
+//  Onwership
+//
+//  Created by Luiz on 2016-09-09.
+//  Copyright © 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import "Triangle.h"
+
+@implementation Triangle
+
+- (void) dealloc
+{
+    NSLog(@"The Triangle %@ has been deallocated.", self);
+}
+
+@end
