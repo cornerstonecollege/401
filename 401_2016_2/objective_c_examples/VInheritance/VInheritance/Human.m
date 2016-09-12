fa3bd2d948cfd1433b1d273c63921ceb2c2b9d44
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-09-12 15:00:43 -0700

    VInheritance

---
+++ 401_2016_2/objective_c_examples/VInheritance/VInheritance/Human.m
@@ -0,0 +1,23 @@
+//
+//  Human.m
+//  VInheritance
+//
+//  Created by Luiz on 2016-09-12.
+//  Copyright © 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import "Human.h"
+
+@implementation Human
+
+- (void) run
+{
+    NSLog(@"Human runs");
+}
+
+- (void) move
+{
+    [self run];
+}
+
+@end
