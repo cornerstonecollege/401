a912043bbaf840af5b288accf98274cbe986549c
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-09-15 17:23:53 -0700

    testing ARC

---
+++ 401_2016_2/objective_c_examples/dfshdfk/dfshdfk/Keyboard.h
@@ -0,0 +1,13 @@
+//
+//  Keyboard.h
+//  dfshdfk
+//
+//  Created by Luiz on 2016-09-15.
+//  Copyright © 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Keyboard : NSObject
+
+@end
---
+++ 401_2016_2/objective_c_examples/dfshdfk/dfshdfk/Mouse.h
@@ -0,0 +1,13 @@
+//
+//  Mouse.h
+//  dfshdfk
+//
+//  Created by Luiz on 2016-09-15.
+//  Copyright © 2016 Ideia do Luiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mouse : NSObject
+
+@end
